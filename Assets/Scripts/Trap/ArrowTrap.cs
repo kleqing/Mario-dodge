3757a188aa7ba4e1fcdf3cbd4df43fe5865085fe
kleqing <hotaruu24k@gmail.com>
2024-12-11 23:55:41 +0700

    Add arrow sound

--- Assets/Scripts/Trap/ArrowTrap.cs
+++ Assets/Scripts/Trap/ArrowTrap.cs
@@ -6,14 +6,17 @@ public class ArrowTrap : MonoBehaviour
     [SerializeField] private float attackCooldown;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject[] Arrows;
+    
+    [SerializeField] private AudioClip arrowSound;
 
     private float cooldownTimer;
     private void Attack()
     {
         cooldownTimer = 0;
-        
+        SoundManager.Instance.PlaySound(arrowSound);
         Arrows[FindArrow()].transform.position = firePoint.position;
         Arrows[FindArrow()].GetComponent<ArrowProjectile>().ActiveProjectile();
+        
     }
 
     private int FindArrow()
